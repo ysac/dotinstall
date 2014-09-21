8990e8769539c963d74b02e4d28b5dbaa08da6c3
ysac <s.yasuhisa@gmail.com>
2014-09-21 22:35:00 +0900

    modify php/quiz bootstrap.sh, index.php

--- php/quiz/bootstrap.sh
+++ php/quiz/bootstrap.sh
@@ -1,14 +1,21 @@
 #!/bin/sh
 
+# 環境を最新に
 yum update -y
+
+# httpdインストール
 yum install httpd -y
 
+# phpインストール
 rpm -Uvh http://rpms.famillecollet.com/enterprise/remi-release-6.rpm
 yum install --enablerepo=remi --enablerepo=remi-php55 php -y
 
+# ドキュメントルートを/vagrantの共有ディレクトリに変更
 rm -rf /var/www/html
 ln -fs /vagrant /var/www/html
 
+# httpd起動設定
+chkconfig --add httpd
 service httpd start
 
 exit 0
--- php/quiz/index.php
+++ php/quiz/index.php
@@ -1,11 +1,14 @@
 <?php
 
+// セッション開始
 session_start();
 
+// エスケープ関数
 function h($s) {
 	return htmlspecialchars($s, ENT_QUOTES, 'UTF-8');
 }
 
+// クイズデータ
 $quizList = array(
 	array(
 		'q' => 'HTMLのHは何の略？',
@@ -25,22 +28,64 @@ $quizList = array(
 	)
 );
 
+// セッションリセット
+function resetSession() {
+	$_SESSION['correct_count'] = 0;
+	$_SESSION['num'] = 0;
+	$_SESSION['token'] = sha1(uniqid(mt_rand(), true));
+}
+
+// リダイレクト
+function redirect() {
+	header('Location: http://'.$_SERVER['HTTP_HOST'].$_SERVER['REQUEST_URI']);
+	exit;	
+}
+
 if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-	if ($_POST['answer'] === $quizList[$_POST['qnum']]['a'][0]) {
-		echo "正解！";
+	// CSRF対策
+	if ($_POST['token'] !== $_SESSION['token']) {
+		echo "不正な投稿です！(1)";
 		exit;
 	}
+
+	if (isset($_POST['reset']) && $_POST['reset'] === '1') {
+		resetSession();
+		redirect();
+	}
+
+	if (!isset($_POST['qnum']) || $_SESSION['qnum'] !== $_POST['qnum']) {
+		echo "不正な投稿です！(2)";
+		exit;
+
+	}
+	if (!isset($quizList[$_POST['qnum']])) {
+		echo "不正な投稿です！(3)";
+		exit;
+	}
+
+	if ($_POST['answer'] === $quizList[$_POST['qnum']]['a'][0]) {
+		// echo "正解！";
+		// exit;
+		$_SESSION['correct_count']++;
+	}
+	$_SESSION['num']++;
+	redirect();
 }
 // var_dump($quizList);
+
+if (empty($_SESSION)) {
+	resetSession();
+}
 
 $qnum = mt_rand(0, count($quizList) - 1);
 $quiz = $quizList[$qnum];
 
-$_SESSON['qnum'] = (string)$qnum;
+$_SESSION['qnum'] = (string)$qnum;
 
 shuffle($quiz['a']);
 
 ?>
+
 <!DOCTYPE html>
 <html lang="ja">
 <head>
@@ -48,12 +93,28 @@ shuffle($quiz['a']);
 	<title>簡単クイズ</title>
 </head>
 <body>
-<p>Q. <?php echo h($quiz['q']); ?></p>
-<?php foreach ($quiz['a'] as $answer) : ?>
+	<div style="padding:7px;background:#eee;border:#ccc">
+		<?php echo h($_SESSION['num']); ?> 問中
+		<?php echo h($_SESSION['correct_count']); ?> 問正解！
+		<?php if ($_SESSION['num'] > 0) : ?>
+		正答率は <?php echo h(sprintf("%.2f", $_SESSION['correct_count'] / $_SESSION['num'] * 100)); ?> % です！
+		<?php endif; ?>
+	</div>
+	<p>Q. <?php echo h($quiz['q']); ?></p>
+	<?php foreach ($quiz['a'] as $answer) : ?>
+		<form action="" method="post">
+			<input type="submit" name="answer" value="<?php echo h($answer); ?>">
+			<input type="hidden" name="qnum" value="<?php echo h($_SESSION['qnum']); ?>">
+			<input type="hidden" name="token" value="<?php echo h($_SESSION['token']); ?>">
+		</form>
+	<?php endforeach; ?>
+
+	<hr>
+
 	<form action="" method="post">
-		<input type="submit" name="answer" value="<?php echo h($answer); ?>">
-		<input type="hidden" name="qnum" value="<?php echo h($_SESSON['qnum']); ?>">
+		<input type="submit" value="リセット">
+		<input type="hidden" name="reset" value="1">
+		<input type="hidden" name="token" value="<?php echo h($_SESSION['token']); ?>">
 	</form>
-<?php endforeach; ?>
 </body>
 </html>
